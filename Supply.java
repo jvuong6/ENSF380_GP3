e41df42deab3f35e6c3bc3b91144d0a3a49fad70
Justin <90071760+jvuong6@users.noreply.github.com>
2025-02-26 12:32:10 -0700

    Add files via upload

---
+++ Supply.java
@@ -0,0 +1,27 @@
+package edu;
+
+public class Supply {
+    private String type;
+    private int quantity;
+
+    public Supply(String type, int quantity) {
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
